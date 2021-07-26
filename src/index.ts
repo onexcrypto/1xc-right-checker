ba266a5b81271a416a74084d76f8bf49de95be8e
themx05 <maximiliencomlan05@gmail.com>
2021-07-26 13:31:47 +0100

    added rights management

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+export * from "./rights";
+
+export const RightChecker = {
+    hasRight (right: string, rights: string[]) {
+        for(let i = 0; i< rights.length; i++ ){
+            let exp = rights[i].replace(".", "\.");
+            if(exp.includes("\.all")) {
+                exp = exp.replace("\.all", "(.*)")
+            }
+            if(right.match(exp)) {
+                return true;
+            }
+        }
+        return false;
+    },
+
+    hasAllRights (search: string[], knownRights: string[] ) {
+        for(let i = 0; i < search.length; i++) {
+            if(!this.hasRight(search[i], knownRights)) {
+                return false
+            }
+        }
+        return true
+    },
+
+    hasAtLeastOneRight (search: string[], knownRights: string[]) {
+        for(let i = 0; i < search.length; i++) {
+            if(this.hasRight(search[i], knownRights)) {
+                return true
+            }
+        }
+        return false
+    },
+
+    canReadUserProfile (rights: string[]) {
+        return this.hasRight("account.profile.read", rights)
+    },
+
+    canEditUserProfile (rights: string[]) {
+        return this.hasRight("account.profile.update", rights)
+    },
+
+    canReadExchanges (rights: string[]) {
+        return this.hasRight("account.exchanges.read", rights)
+    },
+
+    canReadWallet (rights: string[]) {
+        return this.hasRight("account.wallet.read", rights)
+    },
+
+    canUpdateWalletMetadata (rights: string[]) {
+        return this.hasRight("account.wallet.update", rights)
+    },
+
+    canReadWalletHistory (rights: string[]) {
+        return this.hasRight("account.wallet.history.read", rights)
+    },
+
+    canReadWalletTransfers (rights: string[]) {
+        return this.hasRight("account.wallet.transfert.read", rights)
+    },
+
+    canTransferMoney (rights: string[]) {
+        return this.hasRight("account.wallet.transfert.create", rights)
+    },
+
+    canCreateAccessKey (rights: string[]) {
+        return this.hasRight("account.access.create", rights)
+    },
+
+    canReadAccessKeys (rights: string[]) {
+        return this.hasRight("account.access.read", rights)
+    },
+
+    canDeleteAccessKey (rights: string[]) {
+        return this.hasRight("account.access.delete", rights)
+    },
+
+    canReadBusinessDetails (rights: string[]) {
+        return this.hasRight("account.business.read", rights)
+    },
+
+    canUpdateBusinessDetails (rights: string[]) {
+        return this.hasRight("account.business.update", rights)
+    },
+
+    canReadProjects (rights: string[]) {
+        return this.hasRight("account.project.read", rights)
+    },
+
+    canCreateProject (rights: string[]) {
+        return this.hasRight("account.project.create", rights)
+    },
+
+    canUpdateProject (rights: string[]) {
+        return this.hasRight("account.project.update", rights)
+    },
+
+    canDeleteProject (rights: string[]) {
+        return this.hasRight("account.project.delete", rights)
+    },
+
+    canReadBusinessPayments (rights: string[]) {
+        return this.hasRight("account.business.payment.read", rights)
+    },
+
+    canCreatePaymentIntent (rights: string[]) {
+        return this.hasRight("account.business.payment.create-intent", rights)
+    },
+
+}
---
+++ src/rights.ts
@@ -0,0 +1,46 @@
+export const PossibleRights = [
+    "account.all",
+
+    ///Profile rights
+    "account.profile.read",
+    "account.profile.update",
+    "account.profile.all",
+
+    /// exchange rights
+    "account.exchanges.read",
+    'account.exchanges.submit',
+    "account.exchanges.all",
+
+    /// Wallet rights
+    "account.wallet.read",
+    "account.wallet.update",
+    "account.wallet.history.read",
+    "account.wallet.all",
+
+    /// Wallet transfert
+    "account.wallet.transfert.create",
+    "account.wallet.transfert.read",
+    "account.wallet.transfert.all",
+
+    /// Api key
+    "account.access.create",
+    "account.access.read",
+    "account.access.delete",
+    "account.access.all",
+
+    /// Business profile
+    "account.business.read",
+    "account.business.update",
+
+    ///Projects
+    "account.project.read",
+    "account.project.create",
+    "account.project.update",
+    "account.project.delete",
+
+    ///Payment 
+    "account.business.payment.read",
+    "account.business.payment.create-intent",
+    "account.business.payment.all",
+
+]
