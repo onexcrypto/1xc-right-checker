7ead8c797c7ac904708189e7824b883afbf5bb8e
themx05 <maximiliencomlan05@gmail.com>
2021-07-28 15:41:17 +0100

    v0.1.1

--- core/rights.js
+++ core/rights.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.PossibleRights = void 0;
 exports.PossibleRights = [
     "account.all",
     ///Profile rights
